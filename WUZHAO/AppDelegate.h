ba8a314577fa61c8261532dbbb69a3f112c1e723
shenyanping <shenyanping@pinganfang.com>
2014-12-11 14:24:32 +0800

    Initial Commit

---
+++ WUZHAO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WUZHAO
+//
+//  Created by yiyi on 14-12-11.
+//  Copyright (c) 2014年 yiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WUZHAO/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  WUZHAO
+//
+//  Created by yiyi on 14-12-11.
+//  Copyright (c) 2014年 yiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
