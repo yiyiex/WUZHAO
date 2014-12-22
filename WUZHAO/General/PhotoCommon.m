a116401444d15cac92e13f00f00c7d5d6ca171ee
shenyanping <shenyanping@pinganfang.com>
2014-12-22 14:59:07 +0800

    fix the lost file

---
+++ WUZHAO/General/PhotoCommon.m
@@ -0,0 +1,9 @@
+//
+//  PhotoCommon.m
+//  WUZHAO
+//
+//  Created by yiyi on 14-12-22.
+//  Copyright (c) 2014年 yiyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ WUZHAO/General/UIImage+Resize.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Resize.h
+//  WUZHAO
+//
+//  Created by yiyi on 14-12-22.
+//  Copyright (c) 2014年 yiyi. All rights reserved.
+//
+
+#ifndef WUZHAO_UIImage_Resize_h
+#define WUZHAO_UIImage_Resize_h
+
+
+#endif
