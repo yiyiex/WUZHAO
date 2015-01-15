acb3482894268d2e7ebce1be03347eaf9c37be58
shenyanping <shenyanping@pinganfang.com>
2015-01-15 10:54:31 +0800

    fix navigationcontroller bug

---
+++ WUZHAO/Sections/Notice/NoticeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoticeViewController.h
+//  WUZHAO
+//
+//  Created by yiyi on 15-1-14.
+//  Copyright (c) 2015年 yiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoticeViewController : UIViewController
+
+@end
