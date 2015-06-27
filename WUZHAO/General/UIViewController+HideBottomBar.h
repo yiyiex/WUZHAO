d8e6752163ca706b5b85281bd1add3f4ce39f1d3
shenyanping <shenyanping@pinganfang.com>
2015-06-27 17:58:41 +0800

    add google address search

---
+++ WUZHAO/General/UIViewController+HideBottomBar.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HideBottomBar.h
+//  WUZHAO
+//
+//  Created by yiyi on 15/6/26.
+//  Copyright (c) 2015年 yiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (HideBottomBar)
+
+@end
---
+++ WUZHAO/General/UIViewController+HideBottomBar.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HideBottomBar.m
+//  WUZHAO
+//
+//  Created by yiyi on 15/6/26.
+//  Copyright (c) 2015年 yiyi. All rights reserved.
+//
+
+#import "UIViewController+HideBottomBar.h"
+
+@implementation UIViewController (HideBottomBar)
+
+@end
